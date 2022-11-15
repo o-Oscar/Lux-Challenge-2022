8cedee150ef25d011c088edd0b0ab2a98266bfd2
StanislasDOZIAS <72864913+StanislasDOZIAS@users.noreply.github.com>
2022-11-15 17:49:28 +0100

    initialisation de survirvor

---
+++ bots/suvivor/__init__.py
@@ -0,0 +1,7 @@
+from utils.action import ActionHandler
+from utils.obs import Obs
+from utils.reward import Reward
+
+ACTION_HANDLER = ActionHandler.MOVE
+OBS_GENERATOR = Obs.MINIMAL
+REWARD = Reward.SURVIVOR
